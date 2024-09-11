b641327a87c7b6311abab40e8ed2292e1af22455
Minh Huy <huy.mtruong@digicommercegroup.com>
2024-09-11 17:05:04 +0700

    Update function fetchAll, fetchOne

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,18 +1,19 @@
 // apiClient.js
 import axios from 'axios';
 import { BASE_URL } from './constants';
-
-const apiClient = axios.create({
+import _get from 'lodash/get'
+export const apiClient = axios.create({
   baseURL: BASE_URL,
 });
 
-export const fetchAll = async (endpoint) => {
-  const { data } = await apiClient.get(endpoint);
-  return data;
+export const fetchAll = async (endpoint, { pageSize, currentPage }) => {
+  const response = await apiClient.get(`${endpoint}?pageSize=${pageSize}&currentPage=${currentPage}`);
+  const result = _get(response, 'data', null) 
+  return result;
 };
 
-export const fetchOne = async (endpoint, id) => {
-  const { data } = await apiClient.get(`${endpoint}/${id}`);
+export const fetchOne = async (endpoint) => {
+  const { data } = await apiClient.get(`${endpoint}`);
   return data;
 };
 
