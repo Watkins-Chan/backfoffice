05d23e2da982c54ea25ce9adcd2c44f5a589940a
Minh Huy <huy.mtruong@digicommercegroup.com>
2024-09-13 17:26:06 +0700

    Update style alert

--- src/components/alerts/AlertSnackbar.jsx
+++ src/components/alerts/AlertSnackbar.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Snackbar from '@mui/material/Snackbar'
 import MuiAlert from '@mui/material/Alert'
+import { useTheme } from '@mui/material'
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
 })
 
-const AlertSnackbar = ({ open, message, severity = 'success', duration = 6000, onClose }) => {
+const AlertSnackbar = ({ open, message, severity = 'success', duration = 60000, onClose }) => {
+  const theme = useTheme()
   return (
-    <Snackbar open={open} autoHideDuration={duration} onClose={onClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
-      <Alert onClose={onClose} severity={severity}>
+    <Snackbar
+      sx={{ '& .MuiPaper-root': { color: theme.palette.common.white } }}
+      open={open}
+      autoHideDuration={duration}
+      onClose={onClose}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+    >
+      <Alert sx={{ '& .MuiAlert-message': { fontSize: '0.875rem' } }} onClose={onClose} severity={severity}>
         {message}
       </Alert>
     </Snackbar>
