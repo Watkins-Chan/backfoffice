b9ea34fc1891700cc80de211d77fc5a2c4b6a4fa
Minh Huy <huy.mtruong@digicommercegroup.com>
2024-09-23 14:02:47 +0700

    refactor(ui): add pagination, row page selector for author page

--- src/pages/authors/index.jsx
+++ src/pages/authors/index.jsx
@@ -11,6 +11,8 @@ import SearchBar from 'components/common/SearchBar'
 import SortOptions from 'components/common/SortOptions'
 import AddNewButton from 'components/common/AddNewButton'
 import CardInfo from 'components/authors/CardInfo'
+import PaginationControl from 'components/common/PaginationControl'
+import RowPerPageSelector from 'components/common/RowPerPageSelector '
 
 const Authors = () => {
   return (
@@ -38,7 +40,16 @@ const Authors = () => {
             ))}
           </Grid>
         </Box>
-        <Box>3</Box>
+        <Box py={2}>
+          <Grid container spacing={2} justifyContent="space-between" alignItems="center">
+            <Grid item xs={3}>
+              <RowPerPageSelector />
+            </Grid>
+            <Grid item>
+              <PaginationControl />
+            </Grid>
+          </Grid>
+        </Box>
       </Stack>
     </React.Fragment>
   )
