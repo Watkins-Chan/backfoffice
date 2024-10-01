eb6daed4fb012ce13126505db07fbb10a6707dd9
Minh Huy <huy.mtruong@digicommercegroup.com>
2024-10-01 16:40:13 +0700

    feat(manga): create completed

--- src/components/mangas/UpsertMangaModal.jsx
+++ src/components/mangas/UpsertMangaModal.jsx
@@ -4,8 +4,14 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 import { useSearchParams } from 'react-router-dom'
 
+import _get from 'lodash/get'
+import _lowerCase from 'lodash/lowerCase'
+import _forEach from 'lodash/forEach'
+
+import Autocomplete from '@mui/material/Autocomplete'
+import CircularProgress from '@mui/material/CircularProgress'
 import Button from '@mui/material/Button'
-import Checkbox from '@mui/material/Checkbox'
+import Switch from '@mui/material/Switch'
 import LoadingButton from '@mui/lab/LoadingButton'
 import DialogTitle from '@mui/material/DialogTitle'
 import DialogContent from '@mui/material/DialogContent'
@@ -21,27 +27,16 @@ import { CloseOutlined, SaveOutlined } from '@ant-design/icons'
 import BootstrapDialog from 'components/common/modals/BootstrapDialog'
 import BootstrapInput from 'components/common/inputs/BootstrapInput'
 import { useCreateManga } from 'hooks/useMangas'
+import { useAuthors } from 'hooks/useAuthors'
+import { useGenres } from 'hooks/useGenres'
+
+const statusOptions = ['PROCESSING', 'COMPLETED']
 
 const schema = yup.object().shape({
   name: yup.string().required('Name is required'),
-  description: yup.string().required('Description is required'),
-  // status: yup.string().required('Status is required'),
-  // author: yup.string().required('Author is required'),
-  // genres: yup.array().min(1, 'At least one genre is required'),
-  // image: yup.mixed(),
-  // .test('fileSize', 'File size is too large (max 5MB)', (value) => {
-  //   if (!value) return true
-  //   return value.size <= FILE_SIZE
-  // })
-  // .test('fileFormat', 'Unsupported Format (jpg, jpeg, png)', (value) => {
-  //   if (!value) return true
-  //   return SUPPORTED_FORMATS.includes(value.type)
-  // }),
-  // imageUrl: yup.string().url('Must be a valid URL'),
+  description: yup.string(),
+  author: yup.object().required('Author is required'),
 })
-// .test('oneOf', 'Either image or image URL is required', function (value) {
-//   return (value.image && value.imageUrl) || (!value.image && !value.imageUrl)
-// })
 
 const UpsertMangaModal = (props) => {
   const { idManga, handleClose, refetchMangas } = props
@@ -49,10 +44,14 @@ const UpsertMangaModal = (props) => {
   const open = idManga !== null
 
   const { createManga, isLoading: isCreating } = useCreateManga()
-  const [useImageUrl, setUseImageUrl] = useState(false)
+  const { data: authors, isLoading: isGettingAuthors } = useAuthors()
+  const { data: genres, isLoading: isGettingGenres } = useGenres()
 
-  const handleCheckboxUseImageUrl = (event) => {
-    setUseImageUrl(event.target.checked)
+  const [useImageUrl, setUseImageUrl] = useState(false)
+  const [preview, setPreview] = useState(null)
+
+  const resetFields = () => {
+    setPreview(null)
   }
 
   const {
@@ -60,50 +59,72 @@ const UpsertMangaModal = (props) => {
     handleSubmit,
     reset,
     watch,
+    setValue,
+    resetField,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   })
 
+  const handleToggle = () => {
+    setUseImageUrl((prev) => !prev)
+    if (useImageUrl) {
+      resetField('imageUrl')
+      setPreview(null)
+    } else {
+      resetField('image')
+      setPreview(null)
+    }
+  }
+
+  const handleImageUrlChange = (url) => {
+    setPreview(url)
+  }
+
+  const handleFileChange = (file) => {
+    if (file) {
+      const objectUrl = URL.createObjectURL(file)
+      setPreview(objectUrl)
+    } else {
+      setPreview(null)
+    }
+  }
+
   const onReset = () => {
-    reset({ description: '', name: '', file: null })
+    reset({ description: '', name: '', image: null, imageUrl: '', author: null })
+    setUseImageUrl(false)
+    setPreview(null)
   }
 
   const onSubmit = async (data) => {
-    console.log('🚀 ~ onSubmit ~ data:', data)
-    // const formData = new FormData()
-    // formData.append('name', data.name)
-    // formData.append('description', data.description)
-    // formData.append('status', 'processing')
-    // formData.append('author', 'Eiichiro Oda')
-    // // data.genres.forEach((genre) => formData.append('genres[]', genre))
-    // // if (data.image) {
-    // formData.append('image', data.image)
-    // // } else if (data.imageUrl) {
-    // formData.append('imageUrl', data.imageUrl)
-    // // }
-    // try {
-    //   await createManga(formData)
-    //   refetchMangas()
-    //   handleClose()
-    //   onReset()
-    // } catch (error) {
-    //   if (data.image) {
-    //     formData.append('image', data.image)
-    //   }
-    // }
+    const formData = new FormData()
+    formData.append('name', _get(data, 'name', ''))
+    formData.append('description', _get(data, 'description', ''))
+    formData.append('status', _lowerCase(data.status))
+    formData.append('author', _get(data, 'author.author_name', ''))
+    _forEach(_get(data, 'genres', []), (genre) => formData.append('genres[]', _get(genre, 'genre_name', '')))
+    if (useImageUrl) {
+      formData.append('imageUrl', _get(data, 'imageUrl'))
+    } else {
+      formData.append('image', _get(data, 'image'))
+    }
+    await createManga(formData)
+    refetchMangas()
+    handleClose()
   }
 
-  //   useEffect(() => {
-  //     if (!_isEmpty(genre)) {
-  //       reset({ genre_name: _get(genre, 'genre_name', ''), description: _get(genre, 'description', '') })
-  //     }
-  //   }, [genre])
+  useEffect(() => {
+    return () => {
+      if (preview && preview.startsWith('blob:')) {
+        URL.revokeObjectURL(preview)
+      }
+    }
+  }, [preview])
 
   return (
     <BootstrapDialog maxWidth="sm" fullWidth scroll="body" onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
       <DialogTitle sx={{ m: 0, p: 2, fontSize: '1rem', fontWeight: 500 }} id="customized-dialog-title">
-        {idManga ? 'Edit' : 'Create'}
+        {idManga ? 'Edit' : 'Add New'}
       </DialogTitle>
       <IconButton
         aria-label="close"
@@ -148,10 +169,106 @@ const UpsertMangaModal = (props) => {
               render={({ field }) => <BootstrapInput {...field} sx={{ '& > textarea': { width: '100% !important' } }} multiline rows={4} />}
             />
           </FormControl>
+          <FormControl variant="standard" fullWidth>
+            <InputLabel shrink htmlFor="bootstrap-input" sx={{ fontSize: '1rem' }}>
+              Author
+            </InputLabel>
+            <Controller
+              name="author"
+              control={control}
+              render={({ field }) => (
+                <Autocomplete
+                  {...field}
+                  onChange={(_, data) => field.onChange(data)}
+                  sx={{ marginTop: '1.5rem' }}
+                  options={_get(authors, 'data') || []}
+                  getOptionLabel={(option) => option.author_name}
+                  loading={isGettingAuthors}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      id="author-autocomplete"
+                      placeholder="Select Author..."
+                      error={!!errors.author}
+                      helperText={errors.author ? errors.author.message : ''}
+                      InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                          <>
+                            {isGettingAuthors ? <CircularProgress color="inherit" size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                          </>
+                        ),
+                      }}
+                    />
+                  )}
+                />
+              )}
+            />
+          </FormControl>
+          <FormControl variant="standard" fullWidth>
+            <InputLabel shrink htmlFor="bootstrap-input" sx={{ fontSize: '1rem' }}>
+              Genres
+            </InputLabel>
+            <Controller
+              name="genres"
+              control={control}
+              render={({ field }) => (
+                <Autocomplete
+                  {...field}
+                  onChange={(_, data) => field.onChange(data)}
+                  sx={{ marginTop: '1.5rem' }}
+                  multiple
+                  filterSelectedOptions
+                  options={_get(genres, 'data') || []}
+                  getOptionLabel={(option) => option.genre_name}
+                  loading={isGettingGenres}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      id="author-autocomplete"
+                      placeholder="Select Genres..."
+                      error={!!errors.genres}
+                      helperText={errors.genres ? errors.genres.message : ''}
+                      InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                          <>
+                            {isGettingGenres ? <CircularProgress color="inherit" size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                          </>
+                        ),
+                      }}
+                    />
+                  )}
+                />
+              )}
+            />
+          </FormControl>
+          <FormControl variant="standard" fullWidth>
+            <InputLabel shrink htmlFor="bootstrap-input" sx={{ fontSize: '1rem' }}>
+              Status
+            </InputLabel>
+            <Controller
+              name="status"
+              control={control}
+              defaultValue={statusOptions[0]}
+              render={({ field }) => (
+                <Autocomplete
+                  {...field}
+                  onChange={(_, data) => field.onChange(data)}
+                  sx={{ marginTop: '1.5rem' }}
+                  options={statusOptions}
+                  getOptionLabel={(option) => option}
+                  renderInput={(params) => <TextField {...params} placeholder="Select status..." error={!!errors.status} helperText={errors.status ? errors.status.message : ''} />}
+                />
+              )}
+            />
+          </FormControl>
           <FormControlLabel
-            sx={{ '& .MuiCheckbox-root': { padding: 0, marginRight: 1 } }}
-            control={<Checkbox checked={useImageUrl} onChange={handleCheckboxUseImageUrl} inputProps={{ 'aria-label': 'controlled' }} />}
-            label="Use Image URL instead of uploading a file"
+            sx={{ width: 'fit-content' }}
+            control={<Switch checked={useImageUrl} onChange={handleToggle} name="useImageUrl" color="primary" />}
+            label={useImageUrl ? 'Use Image URL' : 'Upload Image'}
           />
           {useImageUrl ? (
             <FormControl variant="standard" fullWidth>
@@ -161,26 +278,35 @@ const UpsertMangaModal = (props) => {
               <Controller
                 name="imageUrl"
                 control={control}
+                defaultValue=""
                 render={({ field }) => (
-                  <BootstrapInput
-                    {...field}
-                    id="image-url-input"
-                    placeholder="Enter Image URL..."
-                    error={!!errors.imageUrl}
-                    helperText={errors.imageUrl ? errors.imageUrl.message : ''}
-                    fullWidth
-                  />
+                  <>
+                    <BootstrapInput
+                      {...field}
+                      id="image-url-input"
+                      placeholder="Enter Image URL..."
+                      error={!!errors.imageUrl}
+                      helperText={errors.imageUrl ? errors.imageUrl.message : ''}
+                      fullWidth
+                      onChange={(e) => {
+                        field.onChange(e.target.value)
+                        handleImageUrlChange(e.target.value)
+                      }}
+                    />
+                    {preview && <img src={preview} alt="Image Preview" style={{ marginTop: '1rem', maxWidth: 250, height: 'auto' }} />}
+                  </>
                 )}
               />
             </FormControl>
           ) : (
-            <FormControl variant="standard" fullWidth>
+            <FormControl variant="standard" sx={{ width: 'fit-content' }}>
               <InputLabel shrink htmlFor="image-upload" sx={{ fontSize: '1rem' }}>
                 Upload Image
               </InputLabel>
               <Controller
                 name="image"
                 control={control}
+                defaultValue={null}
                 render={({ field }) => (
                   <>
                     <input
@@ -189,10 +315,12 @@ const UpsertMangaModal = (props) => {
                       onChange={(e) => {
                         const file = e.target.files[0]
                         field.onChange(file)
+                        handleFileChange(file)
                       }}
                       style={{ marginTop: '1.5rem' }}
                     />
                     {errors.image && <Typography color="error">{errors.image.message}</Typography>}
+                    {preview && <img src={preview} alt="Image Preview" style={{ marginTop: '1rem', maxWidth: 250, height: 'auto' }} />}
                   </>
                 )}
               />
